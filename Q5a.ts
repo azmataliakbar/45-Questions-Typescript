3acd459a6c16cb68d8a1eb771c77be2c4777ce3f
AzmatAliAkbar <144133389+azmataliakbar@users.noreply.github.com>
2024-03-04 01:21:37 +0500

    Add files via upload

---
+++ Q5a.ts
@@ -0,0 +1,11 @@
+/*Q5: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.*/
+
+// the famous person's name and the quote in variables
+let famous_person1: string = "Albert Einstein";
+let quote1: string = "A person who never made a mistake never tried anything new.";
+
+// the message
+let message1: string = (`\n${famous_person1} once said, "${quote1}"`);
+
+// Print the message
+console.log(message1);
