31e18c0ef4a78c4c3ebbd1378066e990bee36255
AzmatAliAkbar <144133389+azmataliakbar@users.noreply.github.com>
2024-03-10 11:26:51 +0500

    Delete Q9.ts
    it was duplicate

--- Q9.ts
+++
@@ -1,8 +0,0 @@
-/*Q9: Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.*/
-
-
-// declaration of variable for number
-let favoriteNumber: number = 9;
-
-// print number with comment
-console.log(`\nMy favorite number is ${favoriteNumber}.`);
