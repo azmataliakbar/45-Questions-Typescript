13804c40e1afaa84e4753d4d52c5ade963adee3b
AzmatAliAkbar <144133389+azmataliakbar@users.noreply.github.com>
2024-03-10 11:27:07 +0500

    Delete Q9.js
    it was duplicate

--- Q9.js
+++
@@ -1,7 +0,0 @@
-/*Q9: Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.*/
-
-// declaration of variable for number
-var favoriteNumber = 9;
-
-// print number with comment
-console.log("\nMy favorite number is ".concat(favoriteNumber, "."));
